40862b1e122737c584908d94eedabd74f1ec7cdb
Joe Bordes <joe@tsolucio.com>
2024-01-19 18:46:17 +0100

    add missing base files

---
+++ modules/PHPListSync/PHPListSync.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'data/CRMEntity.php';
+require_once 'data/Tracker.php';
+
+class PHPListSync extends CRMEntity {
+	public $table_name = 'vtiger_phplistsync';
+	public $table_index= 'phplistsyncid';
+	public $column_fields = array();
+
+	/** Indicator if this is a custom module or standard module */
+	public $IsCustomModule = false;
+	public $HasDirectImageField = false;
+	public $moduleIcon = array('library' => 'standard', 'containerClass' => 'slds-icon_container slds-icon-standard-account', 'class' => 'slds-icon', 'icon'=>'news');
+
+	/**
+	 * Invoked when special actions are performed on the module.
+	 * @param string Module name
+	 * @param string Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
+	 */
+	public function vtlib_handler($modulename, $event_type) {
+		if ($event_type == 'module.postinstall') {
+			// Handle post installation actions
+		} elseif ($event_type == 'module.disabled') {
+			// Handle actions when this module is disabled.
+		} elseif ($event_type == 'module.enabled') {
+			// Handle actions when this module is enabled.
+		} elseif ($event_type == 'module.preuninstall') {
+			// Handle actions when this module is about to be deleted.
+		} elseif ($event_type == 'module.preupdate') {
+			// Handle actions before this module is updated.
+		} elseif ($event_type == 'module.postupdate') {
+			// Handle actions after this module is updated.
+		}
+	}
+}
+?>
